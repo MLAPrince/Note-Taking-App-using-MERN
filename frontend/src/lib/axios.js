7151e13bfec712236dd2c4071772fc0f1d7d7f30
MLAPrince <110454392+MLAPrince@users.noreply.github.com>
2025-08-07 16:04:01 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // const axiosInstance  ===>>> The following function can also be named as this. 
 
-const BASE_URL = import .meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
 
 const api = axios.create({
         // baseURL: "http://localhost:3000/api",
